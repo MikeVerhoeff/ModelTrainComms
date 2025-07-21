0613a4af3aa0d4321d0b6d922fce8771e81d0079
Mike Verhoeff <mike2000v@gmail.com>
2025-07-21 18:48:45 +0200

    Use rust nightly and per-package-target to fix rust-analyzer

--- bin/embedded/src/main.rs
+++ bin/embedded/src/main.rs
@@ -3,18 +3,50 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::{gpio::{Level, Output, Speed}, peripherals::USB_OTG_FS};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
+use embassy_stm32::usb::Driver;
+use embassy_stm32::time::Hertz;
+use embassy_stm32::Config;
 
 #[defmt::panic_handler]
 fn panic() -> ! {
     core::panic!("panic via `defmt::panic!`")
 }
 
+#[embassy_executor::task]
+async fn logger_task(driver: Driver<'static, USB_OTG_FS>) {
+   embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
+}
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.hse = Some(Hse {
+            freq: Hertz(25_000_000),
+            mode: HseMode::Oscillator,
+        });
+        config.rcc.pll_src = PllSource::HSE;
+        config.rcc.pll = Some(Pll {
+            prediv: PllPreDiv::DIV15,
+            mul: PllMul::MUL173,
+            divp: Some(PllPDiv::DIV4), // 25mhz / 15 * 173 / 4 = 72.0833333Mhz.
+            divq: Some(PllQDiv::DIV6), // 25mhz / 15 * 173 / 6 = 48.0555556Mhz.
+            divr: None,
+        });
+        config.rcc.ahb_pre = AHBPrescaler::DIV1;
+        config.rcc.apb1_pre = APBPrescaler::DIV2;
+        config.rcc.apb2_pre = APBPrescaler::DIV1;
+        config.rcc.sys = Sysclk::PLL1_P;
+        config.rcc.mux.clk48sel = mux::Clk48sel::PLL1_Q;
+    }
+    let p = embassy_stm32::init(config);
+    //let p = embassy_stm32::init(Default::default());
+    
+
     info!("Hello World!");
 
     let mut led = Output::new(p.PC13, Level::High, Speed::Low);
