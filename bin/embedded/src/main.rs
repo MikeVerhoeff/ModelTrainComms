3914435ffe24571369dfde1f86d155e936c89714
Mike Verhoeff <mike2000v@gmail.com>
2025-07-28 19:59:48 +0200

    usb serial echo plus message

--- bin/embedded/src/main.rs
+++ bin/embedded/src/main.rs
@@ -1,24 +1,31 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
+use defmt::{panic, *};
 use embassy_executor::Spawner;
-use embassy_stm32::{gpio::{Level, Output, Speed}, peripherals::USB_OTG_FS};
-use embassy_time::Timer;
-use {defmt_rtt as _, panic_probe as _};
-use embassy_stm32::usb::Driver;
+use embassy_futures::join::join;
+use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_stm32::time::Hertz;
-use embassy_stm32::Config;
+use embassy_stm32::usb::{Driver, Instance};
+use embassy_stm32::{bind_interrupts, peripherals, usb, Config};
+use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
+use embassy_usb::driver::EndpointError;
+use embassy_usb::Builder;
+use {defmt_rtt as _, panic_probe as _};
 
 #[defmt::panic_handler]
 fn panic() -> ! {
     core::panic!("panic via `defmt::panic!`")
 }
 
-#[embassy_executor::task]
-async fn logger_task(driver: Driver<'static, USB_OTG_FS>) {
-   embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
-}
+//#[embassy_executor::task]
+//async fn logger_task(driver: Driver<'static, peripherals::USB_OTG_FS>) {
+//   embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
+//}
+
+bind_interrupts!(struct Irqs {
+    OTG_FS => usb::InterruptHandler<peripherals::USB_OTG_FS>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -46,18 +53,133 @@ async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(config);
     //let p = embassy_stm32::init(Default::default());
     
+    let mut onboard_led = Output::new(p.PC13, Level::High, Speed::Low);
+    
+    let mut pin0 = Output::new(p.PB9, Level::High, Speed::Low);
+    let mut pin1 = Output::new(p.PB8, Level::High, Speed::Low);
+    let mut pin2 = Output::new(p.PB7, Level::High, Speed::Low);
+    let mut pin3 = Output::new(p.PB6, Level::High, Speed::Low);
+    let mut pin4 = Output::new(p.PB5, Level::High, Speed::Low);
+    let mut pin5 = Output::new(p.PB4, Level::High, Speed::Low);
+    let mut pin6 = Output::new(p.PB3, Level::High, Speed::Low);
+    let mut pin7 = Output::new(p.PA13, Level::High, Speed::Low);
+    let mut pin8 = Output::new(p.PA0, Level::High, Speed::Low);
+    let mut pin9 = Output::new(p.PA1, Level::High, Speed::Low);
+    let mut pin10 = Output::new(p.PA9, Level::High, Speed::Low);
+    let mut pin11 = Output::new(p.PA8, Level::High, Speed::Low);
+    let mut pin12 = Output::new(p.PB15, Level::High, Speed::Low);
+    let mut pin13 = Output::new(p.PB14, Level::High, Speed::Low);
+    let mut pin14 = Output::new(p.PB13, Level::High, Speed::Low);
+    let mut pin15 = Output::new(p.PB12, Level::High, Speed::Low);
 
-    info!("Hello World!");
+    onboard_led.set_low();
+    pin0.set_low();
+    pin1.set_low();
+    pin2.set_low();
+    pin3.set_low();
+    pin4.set_low();
+    pin5.set_low();
+    pin6.set_low();
+    pin7.set_low();
+    pin8.set_low();
+    pin9.set_low();
+    pin10.set_low();
+    pin11.set_low();
+    pin12.set_low();
+    pin13.set_low();
+    pin14.set_low();
+    pin15.set_low();
 
-    let mut led = Output::new(p.PC13, Level::High, Speed::Low);
+    onboard_led.set_high();
 
+    // Create the driver, from the HAL.
+    let mut ep_out_buffer = [0u8; 256];
+    let mut config = embassy_stm32::usb::Config::default();
+
+    // Do not enable vbus_detection. This is a safe default that works in all boards.
+    // However, if your USB device is self-powered (can stay powered on if USB is unplugged), you need
+    // to enable vbus_detection to comply with the USB spec. If you enable it, the board
+    // has to support it or USB won't work at all. See docs on `vbus_detection` for details.
+    config.vbus_detection = false;
+
+    let driver = Driver::new_fs(p.USB_OTG_FS, Irqs, p.PA12, p.PA11, &mut ep_out_buffer, config);
+
+    // Create embassy-usb Config
+    let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("Embassy");
+    config.product = Some("USB-serial example");
+    config.serial_number = Some("12345678");
+
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    // It needs some buffers for building the descriptors.
+    let mut config_descriptor = [0; 256];
+    let mut bos_descriptor = [0; 256];
+    let mut control_buf = [0; 64];
+
+    let mut state = State::new();
+
+    let mut builder = Builder::new(
+        driver,
+        config,
+        &mut config_descriptor,
+        &mut bos_descriptor,
+        &mut [], // no msos descriptors
+        &mut control_buf,
+    );
+
+    // Create classes on the builder.
+    let mut class = CdcAcmClass::new(&mut builder, &mut state, 64);
+
+    // Build the builder.
+    let mut usb = builder.build();
+
+    // Run the USB device.
+    let usb_fut = usb.run();
+
+    pin0.set_high();
+
+    // Do stuff with the class!
+    let echo_fut = async {
+        loop {
+            pin1.toggle();
+            class.wait_connection().await;
+            info!("Connected");
+            let _ = echo(&mut class, &mut pin3).await;
+            info!("Disconnected");
+            pin2.toggle();
+        }
+    };
+
+    // Run everything concurrently.
+    // If we had made everything `'static` above instead, we could do this using separate tasks instead.
+    join(usb_fut, echo_fut).await;
+}
+
+
+
+struct Disconnected {}
+
+impl From<EndpointError> for Disconnected {
+    fn from(val: EndpointError) -> Self {
+        match val {
+            EndpointError::BufferOverflow => panic!("Buffer overflow"),
+            EndpointError::Disabled => Disconnected {},
+        }
+    }
+}
+
+async fn echo<'d, T: Instance + 'd>(class: &mut CdcAcmClass<'d, Driver<'d, T>>, pin: &mut Output<'_>) -> Result<(), Disconnected> {
+    let mut buf = [0; 64];
+    let before = b"recived: '";
+    let after = b"'\r\n";
     loop {
-        info!("high");
-        led.set_high();
-        Timer::after_millis(300).await;
-
-        info!("low");
-        led.set_low();
-        Timer::after_millis(300).await;
+        let n = class.read_packet(&mut buf).await?;
+        let data = &buf[..n];
+        info!("data: {:x}", data);
+        pin.toggle();
+        class.write_packet(before).await?;
+        class.write_packet(&buf).await?;
+        class.write_packet(after).await?;
+        buf.fill(0u8);
     }
 }
